b51944105ddda2eaa7291084aac6a02e32d31d23
LuukOriginal <luukoriginal@gmail.com>
2024-02-26 18:14:00 +0100

    Room containers added

--- src/pages/Dashboard/pages/Room/Room.jsx
+++ src/pages/Dashboard/pages/Room/Room.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import "./style.css"
 
 export default class Room extends Component {
   render() {
     return (
-      <div>Room</div>
+      <div id="room-page">
+        <div id="room-container-left">
+          <div className='room-card'>
+            info 1
+          </div>
+          <div className='room-card'>
+            info 2
+          </div>
+        </div>
+        <div id="room-container-center">
+          center
+        </div>
+        <div id="room-container-right">
+          right
+        </div>
+      </div>
     )
   }
 }
